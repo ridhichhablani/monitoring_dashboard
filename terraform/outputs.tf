fbb829210e7def239946223823b789aa8d8ea8d0
ridhichhablani <89528072+ridhichhablani@users.noreply.github.com>
2024-08-30 10:24:51 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,8 @@ output "jenkins_public_ip" {
 output "jenkins_url" {
   value = "http://${aws_instance.jenkins_server.public_ip}:8080"
 }
+
+output "grafana_url" {
+  description = "Grafana URL"
+  value       = "http://${aws_instance.jenkins_server.public_ip}:3000"
+}
